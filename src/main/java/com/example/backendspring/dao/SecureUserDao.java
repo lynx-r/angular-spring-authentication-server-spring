ecc79b62e83736b160ada5429e6bc5e6ba319f7b
Aleksey <aleksey.popryaduhin@yahoo.com>
2018-04-30 09:25:42 +0300

    Исправлены ошибки

--- src/main/java/com/example/backendspring/dao/SecureUserDao.java
+++ src/main/java/com/example/backendspring/dao/SecureUserDao.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Field;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 @Service
 public class SecureUserDao extends BaseDao<SecureUser> {
@@ -23,23 +22,21 @@ public class SecureUserDao extends BaseDao<SecureUser> {
   }
 
   private Optional<SecureUser> findByAttributeIndex(String attribute, String attributeName) {
-    Stream<SecureUser> stream = db.values().stream().map((sUser) -> {
-      try {
-        Field declaredField = sUser.getClass().getDeclaredField(attributeName);
-        declaredField.setAccessible(true);
-        boolean found = declaredField.get(sUser).equals(attribute);
-        if (found) {
-          return sUser;
-        }
-      } catch (NoSuchFieldException | IllegalAccessException e) {
-        System.out.println("ERROR: Пользователь не найден " + sUser);
-      }
-      return null;
-    });
-    SecureUser[] objects = stream.toArray(SecureUser[]::new);
-    if (objects.length == 0) {
-      return Optional.empty();
-    }
-    return Optional.ofNullable(objects[0]);
+    return db.values()
+        .stream()
+        .filter((sUser) -> {
+          try {
+            Field declaredField = sUser.getClass().getDeclaredField(attributeName);
+            declaredField.setAccessible(true);
+            boolean found = declaredField.get(sUser).equals(attribute);
+            if (found) {
+              return true;
+            }
+          } catch (NoSuchFieldException | IllegalAccessException e) {
+            System.out.println("ERROR: Не верное поле " + attributeName + " " + attribute);
+          }
+          return false;
+        })
+        .findFirst();
   }
 }
